1dc1eab6345c415239029d6951a4eae53cd39c78
4business12312025-maker <4business12312025@gmail.com>
2026-06-06 18:59:07 +0800

    Update codebook.py

--- tokenizer/codebook.py
+++ tokenizer/codebook.py
@@ -1,1 +1,17 @@
+import torch
+import torch.nn as nn
 
+
+class Codebook(nn.Module):
+    def __init__(self,
+                 num_embeddings=512,
+                 embedding_dim=128):
+        super().__init__()
+
+        self.embedding = nn.Embedding(
+            num_embeddings,
+            embedding_dim
+        )
+
+    def forward(self, z):
+        return z
