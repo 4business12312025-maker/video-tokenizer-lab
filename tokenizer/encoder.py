bc61df34d682d1d51158b4092307e3f291146930
4business12312025-maker <4business12312025@gmail.com>
2026-06-06 18:54:58 +0800

    Update encoder.py

--- tokenizer/encoder.py
+++ tokenizer/encoder.py
@@ -1,1 +1,21 @@
+import torch
+import torch.nn as nn
 
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 32, 4, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(32, 64, 4, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 128, 4, stride=2, padding=1),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.net(x)
