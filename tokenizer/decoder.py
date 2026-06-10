d86414aa897f89e2a4e140ff2e5f7da0baef0f22
4business12312025-maker <4business12312025@gmail.com>
2026-06-10 23:04:33 +0800

    Update decoder.py

--- tokenizer/decoder.py
+++ tokenizer/decoder.py
@@ -1,20 +1,21 @@
-import torch
 import torch.nn as nn
 
 class Decoder(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.net = nn.Sequential(
-            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1),
+        self.model = nn.Sequential(
+            nn.Linear(256, 128 * 8 * 8),
             nn.ReLU(),
+            nn.Unflatten(1, (128, 8, 8)),
 
-            nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1),
+            nn.ConvTranspose2d(128, 64, 4, 2, 1),  # 8 → 16
             nn.ReLU(),
-
-            nn.ConvTranspose2d(32, 3, 4, stride=2, padding=1),
+            nn.ConvTranspose2d(64, 32, 4, 2, 1),   # 16 → 32
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 3, 4, 2, 1),    # 32 → 64
             nn.Sigmoid()
         )
 
-    def forward(self, z):
-        return self.net(z)
+    def forward(self, x):
+        return self.model(x)
