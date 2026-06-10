20280811672bac35733d9742117a7456bcbab582
4business12312025-maker <4business12312025@gmail.com>
2026-06-10 20:03:15 +0800

    Update decoder.py

--- tokenizer/decoder.py
+++ tokenizer/decoder.py
@@ -1,1 +1,20 @@
+import torch
+import torch.nn as nn
 
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(64, 32, 4, stride=2, padding=1),
+            nn.ReLU(),
+
+            nn.ConvTranspose2d(32, 3, 4, stride=2, padding=1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, z):
+        return self.net(z)
