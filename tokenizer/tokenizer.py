fb980c58f2ab48d0ea6b9fe908dc52596e467249
4business12312025-maker <4business12312025@gmail.com>
2026-06-10 20:19:46 +0800

    Update tokenizer.py

--- tokenizer/tokenizer.py
+++ tokenizer/tokenizer.py
@@ -1,1 +1,21 @@
+from encoder import Encoder
+from decoder import Decoder
+from codebook import Codebook
 
+class Tokenizer(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.encoder = Encoder()
+        self.codebook = Codebook()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+
+        z = self.encoder(x)
+
+        z_q = self.codebook(z)
+
+        x_hat = self.decoder(z_q)
+
+        return x_hat
