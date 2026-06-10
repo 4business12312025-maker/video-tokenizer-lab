cbcbe0713a1ddb6e52a03555dbcb9108cc2779d2
4business12312025-maker <4business12312025@gmail.com>
2026-06-10 23:07:01 +0800

    Update Model.py

--- tokenizer/Model.py
+++ tokenizer/Model.py
@@ -1,1 +1,17 @@
 
+from encoder import Encoder
+from decoder import Decoder
+
+class Autoencoder:
+    def __init__(self, device):
+        self.encoder = Encoder().to(device)
+        self.decoder = Decoder().to(device)
+
+    def parameters(self):
+        return list(self.encoder.parameters()) + list(self.decoder.parameters())
+
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, z):
+        return self.decoder(z)
